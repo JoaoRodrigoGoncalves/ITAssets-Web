33f46130f67d31c69447969e1dbe08240f6bc24f
Inês <92525568+InesArnauth@users.noreply.github.com>
2023-01-12 20:08:04 +0000

    EndPoint PedidoAlocação

--- backend/modules/api/controllers/PedidoalocacaoController.php
+++ backend/modules/api/controllers/PedidoalocacaoController.php
@@ -2,6 +2,8 @@
 
 namespace backend\modules\api\controllers;
 
+use common\models\Grupoitens;
+use common\models\Item;
 use common\models\PedidoAlocacao;
 use Yii;
 use yii\filters\auth\HttpBearerAuth;
@@ -37,7 +39,7 @@ class PedidoalocacaoController extends ActiveController
     public function actions()
     {
         $actions = parent::actions();
-        unset($actions['delete']);
+        unset($actions['delete'], $actions['update'], $actions['create']);
         return $actions;
     }
 
@@ -97,6 +99,150 @@ class PedidoalocacaoController extends ActiveController
         }
     }
 
+    public function actionCreate(){
+        //Cria um Novo Pedido de Alocação
+        $model = new PedidoAlocacao();
+
+        $this->checkAccess("create", null);
+
+        $data = Yii::$app->getRequest()->getBodyParams();
+
+        //Verifica se foi inserido um aprovador e guarda o status: caso inserido o estado é aprovado, caso não inserido o estado é aberto
+        if(isset($data['aprovador_id'])){
+            $model->status = PedidoAlocacao::STATUS_APROVADO;
+        }else{
+            $model->status = PedidoAlocacao::STATUS_ABERTO;
+        }
+
+        //Guarda os seguintes campos
+        $model->requerente_id = $data['requerente_id'];
+        $model->aprovador_id = $data['aprovador_id'];
+        $model->obs = $data['obs'];
+        $model->obsResposta = $data['obsResposta'];
+
+        //Valida se foi colocado um item e um grupo no mesmo pedido: caso aconteça tem de dar erro
+        if(isset($data['item_id']) && isset($data['grupoItem_id'])){
+            throw new BadRequestHttpException("Não é possível selecionar um item e um grupo de itens ");
+        }
+
+        //Valida se o item exite na base de dados
+        //Valida se o item não está associado num grupo de itens nem alocado
+        if(isset($data['item_id'])){
+            $item = Item::findOne($data['item_id']);
+
+            //ve se o item existe na bd
+            if ($item != null) {
+                //O Item não pode estar num grupo de itens nem alocado
+                if (!$item->isInActiveItemsGroup() && !$item->isInActivePedidoAlocacao()) {
+                    $model->item_id = $data['item_id'];
+                    $model->grupoItem_id = null;
+
+                    $model->save();
+                    return $model;
+                } else {
+                    throw new BadRequestHttpException("O Item não pode estar associado a um Grupo de itens e nem Alocado");
+                }
+            }
+            else{
+                throw new BadRequestHttpException("O Item não existe");
+            }
+        }
+        //Valida se o grupo de itens exite na base de dados
+        //Valida se o grupo de itens não está alocado
+        elseif(isset($data['grupoItem_id'])){
+            $grupoItens = Grupoitens::findOne($data['grupoItem_id']);
+
+            //ve se o grupo de itens existe na bd
+            if ($grupoItens != null) {
+                //O Grupo de Itens não pode estar alocado
+                if (!$grupoItens->isinActivePedidoAlocacao()) {
+                    $model->item_id = null;
+                    $model->grupoItem_id = $data['grupoItem_id'];
+
+                    $model->save();
+                    return $model;
+                } else {
+                    throw new BadRequestHttpException("O Grupo de Item não pode estar Alocado");
+                }
+            }
+            else{
+                throw new BadRequestHttpException("O Grupo de Itens não existe");
+            }
+        }
+    }
+
+    public function actionUpdate($id){
+        $model = PedidoAlocacao::findOne(['id' => $id]);
+
+        if($model != null)
+        {
+            $this->checkAccess("update", null, ['id' => $id]);
+            $data = Yii::$app->getRequest()->getBodyParams();
+
+            switch ($data['status']){
+                /** Caso o status seja igual a 9, ou seja, APROVADO **/
+                case PedidoAlocacao::STATUS_APROVADO:
+                    if($model->status == PedidoAlocacao::STATUS_ABERTO)
+                    {
+                        $model->status = PedidoAlocacao::STATUS_APROVADO;
+                        $model->aprovador_id = $data['aprovador_id'];
+                        $model->obsResposta = $data['obsResposta'];
+                        $model->save();
+                    }
+                    else{
+                        throw new BadRequestHttpException("Não é possível aprovar um pedido depois de processado");
+                    }
+                    break;
+
+                /** Caso o status seja igual a 7, ou seja, DEVOLVIDO **/
+                case PedidoAlocacao::STATUS_DEVOLVIDO:
+                    if($model->status == PedidoAlocacao::STATUS_APROVADO)
+                    {
+                        $model->status = PedidoAlocacao::STATUS_DEVOLVIDO;
+                        $model->save();
+                    }
+                    else
+                    {
+                        throw new BadRequestHttpException("Não é possível devolver um pedido depois de processado");
+                    }
+                    break;
+
+                /** Caso o status seja igual a 0, ou seja, CANCELADO **/
+                case PedidoAlocacao::STATUS_CANCELADO:
+                    if($model->status == PedidoAlocacao::STATUS_ABERTO)
+                    {
+                        $model->status = PedidoAlocacao::STATUS_NEGADO;
+                        $model->save();
+                    }
+                    else
+                    {
+                        throw new BadRequestHttpException("Não é possível cancelar um pedido depois de processado");
+                    }
+                    break;
+
+                /** Caso o status seja igual a 8, ou seja, NEGADO **/
+                case PedidoAlocacao::STATUS_NEGADO:
+                    if($model->status == PedidoAlocacao::STATUS_ABERTO)
+                    {
+                        $model->status = PedidoAlocacao::STATUS_NEGADO;
+                        $model->aprovador_id = $data['aprovador_id'];
+                        $model->obsResposta = $data['obsResposta'];
+                        $model->save();
+                    }
+                    else
+                    {
+                        throw new BadRequestHttpException("Não é possível negar um pedido depois de processado");
+                    }
+                    break;
+            }
+            return $model;
+        }
+        else
+        {
+            throw new NotFoundHttpException("Pedido de Alocação não encontrado");
+        }
+    }
+
     public function actionDelete($id)
     {
         $model = PedidoAlocacao::findOne(['id' => $id]);
@@ -107,7 +253,7 @@ class PedidoalocacaoController extends ActiveController
 
             if($model->status == PedidoAlocacao::STATUS_ABERTO)
             {
-                $model->status = 0;
+                $model->status = PedidoAlocacao::STATUS_CANCELADO;
                 $model->save();
             }
             else
